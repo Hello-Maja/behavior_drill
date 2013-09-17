831bc6cfe741298d87b6610aeed0e065eed7d95b
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-16 22:52:09 -0500

    .

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $('.sortable-list').sortable();
+    // getter
+    var axis = $( ".sortable-list" ).sortable( "option", "axis" );
+    // setter
+    $( ".sortable-list" ).sortable( "option", "axis", "y" );
 });
